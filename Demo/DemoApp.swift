40f2292269f13aac2020b70f52bf8b6c50df3ca4
Prayag Gediya <prayaggediya60@gmail.com>
2021-06-30 14:48:47 +0530

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Prayag Gediya on 30/06/21.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
